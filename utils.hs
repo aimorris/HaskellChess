d9b923e0c5d90f2d6780c1d79b812d8cd6ada1fa
Adam Morris <aimorris@aimorris.com>
2023-12-03 18:31:50 +1000

    king attacks

--- utils.hs
+++ utils.hs
@@ -1,23 +1,61 @@
 {-# LANGUAGE BinaryLiterals #-}
-import Data.Bits ( Bits((.|.), (.&.), shift, complement) )
+import Data.Bits
 import Foreign.C.Types (CULong)
 import Data.Int (Int64)
 
 type Bitboard = CULong
 
+-- Definitions of empty bitboard, files, and ranks bitboards
+
+emptyBoard :: Bitboard
+emptyBoard = zeroBits
+
 fileBitboard :: Int -> Bitboard
 fileBitboard = shift 0b100000001000000010000000100000001000000010000000100000001
 
-getKnightAttackBitboardSingle :: Bitboard -> Int -> Bitboard -> Bitboard
-getKnightAttackBitboardSingle board shiftAmount mask = (.&.) (shift board shiftAmount) (complement mask)
+rankBitboard :: Int -> Bitboard
+rankBitboard = shift 0b11111111
 
-getKnightAttackBitboard :: Bitboard -> Bitboard
-getKnightAttackBitboard board =
-    getKnightAttackBitboardSingle board 10 (fileBitboard 0 .|. fileBitboard 1) .|.
-    getKnightAttackBitboardSingle board 17 (fileBitboard 0) .|.
-    getKnightAttackBitboardSingle board 15 (fileBitboard 7) .|.
-    getKnightAttackBitboardSingle board 6 (fileBitboard 6 .|. fileBitboard 7) .|.
-    getKnightAttackBitboardSingle board (-10) (fileBitboard 6 .|. fileBitboard 7) .|.
-    getKnightAttackBitboardSingle board (-17) (fileBitboard 7) .|.
-    getKnightAttackBitboardSingle board (-15) (fileBitboard 0) .|.
-    getKnightAttackBitboardSingle board (-6) (fileBitboard 0 .|. fileBitboard 1)
+-- Shifts and neighbours
+
+getShiftWithMask :: Bitboard -> Int -> Bitboard -> Bitboard
+getShiftWithMask board shiftAmount mask = shift board shiftAmount .&. complement mask
+
+northNeighbour :: Bitboard -> Bitboard
+northNeighbour board = getShiftWithMask board 8 (rankBitboard 0)
+
+southNeighbour :: Bitboard -> Bitboard
+southNeighbour board = getShiftWithMask board (-8) (rankBitboard 7)
+
+eastNeighbour :: Bitboard -> Bitboard
+eastNeighbour board = getShiftWithMask board 1 (fileBitboard 0)
+
+westNeighbour :: Bitboard -> Bitboard
+westNeighbour board = getShiftWithMask board (-1) (fileBitboard 7)
+
+-- Piece attack bitboards
+
+getSingleKnightAttackBitboard :: Bitboard -> Maybe Bitboard
+getSingleKnightAttackBitboard board
+    | popCount board > 1 = Nothing
+    | otherwise = Just $
+        getShiftWithMask board 10 (fileBitboard 0 .|. fileBitboard 1) .|.
+        getShiftWithMask board 17 (fileBitboard 0) .|.
+        getShiftWithMask board 15 (fileBitboard 7) .|.
+        getShiftWithMask board 6 (fileBitboard 6 .|. fileBitboard 7) .|.
+        getShiftWithMask board (-10) (fileBitboard 6 .|. fileBitboard 7) .|.
+        getShiftWithMask board (-17) (fileBitboard 7) .|.
+        getShiftWithMask board (-15) (fileBitboard 0) .|.
+        getShiftWithMask board (-6) (fileBitboard 0 .|. fileBitboard 1)
+
+getSingleKingAttackBitboard :: Bitboard -> Maybe Bitboard
+getSingleKingAttackBitboard board
+    | popCount board > 1 = Nothing
+    | otherwise = Just $ xor board $
+        eastNeighbour northSouthNeighbours .|.
+        westNeighbour northSouthNeighbours .|.
+        northSouthNeighbours
+    where northSouthNeighbours =
+            northNeighbour board .|.
+            southNeighbour board .|.
+            board
